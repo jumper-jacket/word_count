e7c9de86f5936b263760acd86116a573f7f6b136
User <u96464224@gmail.com>
2024-09-05 11:38:37 +0900

    ファイルが存在するか確かめる機能の追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    println!("file name: ");
+
+    let mut file_name = String::new();
+    io::stdin().read_line(&mut file_name)
+    .expect("failed");
+
+    let file_name = file_name.trim();
+    let path = Path::new(file_name);
+
+    if path.exists() {
+        println!("exist {}", file_name);
+    } else {
+        println!("does not exist {}", file_name);
+    }
 }
